65a021a54b62f6de5cad8c77d435160543b03a81
giulianaxd2 <46542579+giulianaxd2@users.noreply.github.com>
2019-02-20 15:12:54 -0500

    procedimientos almacenados
    procediminetos para los reportes

--- Base de datos Daniel.sql
+++ Base de datos Daniel.sql
@@ -1143,7 +1143,7 @@ begin
 end
 go
 exec spuDiasMasVendidos
---------SPU para saber los clientes que gastan mas dentro de la tienda xdxdxdx
+--------SPU para saber los clientes que gastan mas dentro de la tienda xdxdxd
 if exists (select * from dbo.sysobjects where name='spuClientesEstrella')
 	drop procedure spuClientesEstrella
 go
